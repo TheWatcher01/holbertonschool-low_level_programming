5956ec578883dede159e979e8de9b08de42f7864
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-13 05:44:50 -0700

    Repo_for_c_language_project

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
