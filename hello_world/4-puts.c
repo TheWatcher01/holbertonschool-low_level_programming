3fa1f790baa9ce91cc86db5f66f180f81f3336ad
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-13 05:34:24 -0700

    Repo_for_c_language_project

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/*
+ * File: 4-puts.c
+ * Auth: Teddy Deberdt
+ */
+
+/*
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
