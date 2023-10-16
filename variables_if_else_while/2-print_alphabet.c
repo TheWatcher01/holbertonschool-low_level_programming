2724925794bb1b6083d0cf6702613c5fe5ff408c
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 04:50:37 -0700

    Commit for variables courses

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Print the alphabet in lowercase
+ *
+ * Return 0 (sucess)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	while (letter < 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
