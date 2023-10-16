13042ac2e0e3fd13523d7d70de2d56f8508f4280
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 01:53:00 -0700

    First_exercise_commit_variable_if_else_while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 	printf("%d",n);
 	if ( n > 0 ) 
 	{
-		printf("is positive \n");
+		printf("%d is positive\n",n);
 	} else if (n == 0) {
-		printf("is zero \n");
+		printf("%d is zero\n",n);
 	} else {
-		printf("is negative \n");
+		printf("%d is negative\n",n);
 	}
 	return (0);
 	
