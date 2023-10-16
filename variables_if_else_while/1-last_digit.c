184f86fea41c1111d8193f956eaf2714952efd16
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 03:19:49 -0700

    First_exercise_commit_variable_if_else_while

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,13 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 
-//
+// main - Print the last digit of the number stored in the variable n
+// Return: 0 (sucess) 
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	
+	printf("%Last digit of\n", n );
+
+
 	return (0);
 }
