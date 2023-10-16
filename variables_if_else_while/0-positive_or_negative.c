d90733ddd09b50c765fc3aa6f8954becd43cf7f3
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 01:31:00 -0700

    First_exercise_commit_variable_if_else_while

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d",n);
+	if ( n > 0 ) {
+		printf("is positive\n");
+	} else if (n == 0) {
+		printf("is zero\n");
+	} else {
+		printf("is negative\n");
+	}
+	return (0);
+	
+}
