a4a4d9f22393e53da9ade8baebc513877292cea5
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 06:21:51 -0700

    Commit for variables courses

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10 from 0, followed by a new line.
+ * main - Prints all single digit numbers of base 10 from 0
  *
  * Return: 0 (sucess)
  */
