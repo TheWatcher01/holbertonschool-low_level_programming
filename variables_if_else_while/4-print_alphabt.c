f54c047c836827bee6dee79219da5e29cc29868a
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 05:55:31 -0700

    Commit for variables courses

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: value of main 0 (sucess)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z';
+	{
+		if (letter != 'q' && letter !='e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n')
+
+	return (0);
+}
