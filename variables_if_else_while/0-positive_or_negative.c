2efbee7c0138722bd051b79132f6747522e09081
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 01:48:31 -0700

    First_exercise_commit_variable_if_else_while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	printf("%d",n);
-	if ( n > 0 ) {
-		printf("is positive \n");
+	if ( n > 0 ) 
+	{
+		printf("%is positive \n");
 	} else if (n == 0) {
-		printf("is zero \n");
+		printf("%is zero \n");
 	} else {
-		printf("is negative \n");
+		printf("%is negative \n");
 	}
 	return (0);
 	
