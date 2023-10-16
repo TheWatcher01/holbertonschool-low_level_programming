891090c77a52dd8d4ad5f4055b7e0a257b64029f
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 01:59:02 -0700

    First_exercise_commit_variable_if_else_while

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * main - Prints if number is positive, zero or negative
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
 	int n;
