1e4404ee05c00b41370ca17603c7e7b9a3c22ede
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-16 03:41:24 -0700

    First_exercise_commit_variable_if_else_while

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,8 +12,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	printf("%Last digit of\n", n );
+	printf("% Last digit of %d is", n );
 
+	if (n % > 5)
+	{
+		printf("%d and is greater than 5\n", n % 10);
+	}
+
+	else if (n % 10 == 0)
+	{
+		printf("%d and is 0\n", n % 10);
+	}
+	else 
+	{
+		printf("%d and is less than 6 and not 0\n", n % 10);
+	}
 
 	return (0);
 }
