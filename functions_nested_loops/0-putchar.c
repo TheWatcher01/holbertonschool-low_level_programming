1185122c217615c728e6f6d0e5862320da400992
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-17 02:02:59 -0700

    last update commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Entry point
+ * main -  program that prints _putchar, followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+        _putchar('r');
+        _putchar('\n');
+
 	return 0;	
 }
