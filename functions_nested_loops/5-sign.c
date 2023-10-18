68d4314a18758e153580edd174d29bff5241f68f
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 07:03:46 -0700

    next commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * print_sign - prints the sign of a number
-*
+* @n: The number of which the sign will be printed.
 * Return: 1 and prints + if n is greater than zero
 * 0 and prints 0 if n is zero
 * -1 and prints - if n is less than zero
