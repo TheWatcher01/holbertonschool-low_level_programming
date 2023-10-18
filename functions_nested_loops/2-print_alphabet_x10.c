4b74009509514134a63b6d90b6fdd5758aad6947
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 00:50:14 -0700

    Commit for functions nested loops project

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - Print the alphabet in lowercase, x10
+ *
+ * Return: value of main 0 (sucess)
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i = 0;
+
+	while (i <= 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	
+		_putchar('\n');
+		letter = 'a';
+		i++;
+
+	return (0);
+}
