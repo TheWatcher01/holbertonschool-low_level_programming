4869494a39e3fb4cf82ccf4bb3df95ccd5334553
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 05:13:30 -0700

    next commit

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1 for validation
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}i
--- functions_nested_loops/4-main.c
+++ functions_nested_loops/4-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     int r;
