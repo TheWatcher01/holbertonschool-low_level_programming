6e18f10aa94bbc3a25c8718118493418c943925f
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 07:01:38 -0700

    next commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
 * print_sign - prints the sign of a number
 *
 * Return: 1 and prints + if n is greater than zero
-* Return: 0 and prints 0 if n is zero
-* Return: -1 and prints - if n is less than zero
+* 0 and prints 0 if n is zero
+* -1 and prints - if n is less than zero
 */
 int print_sign(int n)
 {
