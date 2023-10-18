91bb11ed66181e9cf9fba330733646c0969f505d
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 07:43:41 -0700

    next commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,12 @@
 int print_last_digit(int num)
 {
 	int last_digit = num % 10;
+
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
 	}
-	putchar (last_digit + '0');
+	_putchar (last_digit + '0');
 	return (last_digit);
 
 }
