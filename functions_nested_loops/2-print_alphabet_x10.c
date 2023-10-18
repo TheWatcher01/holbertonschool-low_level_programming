2913da9e0b9b88b90d1ba2393b5b251282888d67
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 00:56:36 -0700

    Commit for functions nested loops project

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,17 +10,15 @@ void print_alphabet_x10(void)
 	char letter = 'a';
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
 		}
-	
 		_putchar('\n');
 		letter = 'a';
 		i++;
-
-	return (0);
+	}
 }
