315bd3fad2d1df00ec7a00f576e9a384fb3fd00b
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 06:27:43 -0700

    next commit

--- functions_nested_loops/4-main.c
+++ functions_nested_loops/4-main.c
@@ -4,6 +4,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     int r;
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -12,4 +12,5 @@
 	int _putchar(char);
 	void print_alphabet_x10(void);
 	int _islower(int c);
+	int _isalpha(int c);
 #endif
