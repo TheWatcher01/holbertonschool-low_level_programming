76cb328c1d7a63ea507803ef7571e3e89f8d4a26
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 06:48:39 -0700

    next commit

--- functions_nested_loops/5-main.c
+++ functions_nested_loops/5-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - check the code.
  *
--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,2 +1,26 @@
 #include "main.h"
-
+/**
+* print_sign - prints the sign of a number 
+*
+* Return: 1 and prints + if n is greater than zero
+* Return: 0 and prints 0 if n is zero
+* Return: -1 and prints - if n is less than zero
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return 1;
+	}
+	else if (n == 0)
+	{
+		putchar(n == 0);
+		return 0;
+	else
+	{
+		putchar('-');
+		return -1;
+	}
+	}
+}
