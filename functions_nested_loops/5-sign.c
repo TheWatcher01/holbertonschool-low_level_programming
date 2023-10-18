4e9afca88011cdf93e41d9f76dd7f1ee7678cdd6
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 06:58:08 -0700

    next commit

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,18 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		putchar(n + '0');
-		return 1;
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(n == 0);
-		return 0;
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return -1;
 	}
 	
