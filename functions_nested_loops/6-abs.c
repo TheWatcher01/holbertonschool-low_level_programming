a82fea652dd97102d494e4df551c33c23be304f2
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-18 07:16:29 -0700

    next commit

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int _abs -  function that computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ *Return: The absolute value of the integer.
+ */
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return -num;
+	}
+	else
+	{
+		return num;
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -14,4 +14,5 @@
 	int _islower(int c);
 	int _isalpha(int c);
 	int print_sign(int n);
+	int _abs(int);
 #endif
