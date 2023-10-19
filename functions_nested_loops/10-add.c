df88ba1786446f50fceaaaf14f16315f000aa04d
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-19 06:39:35 -0700

    the next commit

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
 # include <stdio.h>
 # include "main.h"
 /**
- * int add - Function that adds two integers and returns the result.
- *
+ * add - Function that adds two integers and returns the result.
+ * @a: first int
+ * @b: second int
  * Return: Result of adddition
  */
 int add(int a, int b)
