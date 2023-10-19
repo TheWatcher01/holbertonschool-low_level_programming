1e9ba4bbd8294967ca30c64fc0c245ea519c1fc4
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-19 02:09:49 -0700

    next commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@ void jack_bauer(void)
 
 	for (hour = 0; hour < 24; hour++)
 	{
-		for (minute = 0; minute < 24; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
 			putchar(hour / 10 + '0');
 			putchar(hour % 10 + '0');
