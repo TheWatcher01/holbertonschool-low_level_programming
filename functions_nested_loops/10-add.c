3d2771845f3ec49a850dd9f88a822e61fde102f7
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-19 06:34:41 -0700

    the next commit

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include "main.h"
+/**
+ * int add - Function that adds two integers and returns the result.
+ * 
+ * Return: Result of adddition
+ */
+int add (int a, int b)
+{
+	return a + b;
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -18,4 +18,5 @@
 	int print_last_digit(int);
 	void jack_bauer(void);
 	void times_table(void);
+	int add(int, int);
 #endif
