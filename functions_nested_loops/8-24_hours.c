23acf5518535690a8f1318551721bcc8e0c5594b
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-19 02:13:29 -0700

    next commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * jack_bauer - function list every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ *
+ */ 
 void jack_bauer(void)
 {
 	int hour, minute;
