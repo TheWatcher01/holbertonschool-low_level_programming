11afaddb4d42b468f9265f29c1ab42f0438cae81
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-19 05:55:42 -0700

    the next commit

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,15 +16,15 @@ void times_table(void)
 
 			if (reste >= 10)
 			{
-				putchar(44);
-				putchar(32);
-				putchar(32);
+				putchar(",");
+				putchar(" ");
+				putchar(" ");
 				putchar(reste + '0');
 			}
 			else
 			{
-				putchar(44);
-				putchar(32);
+				putchar(",");
+				putchar(" ");
 				putchar((reste / 10) + '0');
 				putchar((reste % 10) + '0');
 			}
