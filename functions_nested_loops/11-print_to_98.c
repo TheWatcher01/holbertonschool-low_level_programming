3e4871aeb3a625576693b21e6ac80971f5c91f04
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-20 00:25:43 -0700

    next commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,14 +6,20 @@
   *
   * Return: Always 0.
   */
-void print_to_98(int n )
+void print_to_98(int n)
 {
-	int i;
 	if (n <= 98)
 	{
-		for (i = n; i < 98; i++)
+		for (int i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
+		}
+		else
+		{
+			for (int i = n; i > 98; i--)
+			{
+				printf("%d, ", i);
+			}
 		}
 	}
 	printf("98\n");
