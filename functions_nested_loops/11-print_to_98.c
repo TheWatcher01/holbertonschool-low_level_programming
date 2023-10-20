3c86c6e8ef12745ddc0c80d82ecf7d2a79da435b
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-20 00:13:27 -0700

    next commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
   * print_to_98 - Prints all natural numbers from n to 98
   * @n: The number to start printing from
@@ -16,14 +17,4 @@ void print_to_98(int n )
 		}
 	}
 	printf("98\n");
-}
-
-int main(void) 
-{
-  print_to_98(0);
-  print_to_98(98);
-  print_to_98(111);
-  print_to_98(81);
-  print_to_98(-10);
-  return 0;
 }
