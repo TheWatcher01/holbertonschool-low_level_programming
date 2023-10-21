b07eaa461f2cc6d5f680d44d8c39aee40a023a83
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 08:50:29 -0700

    Commit for C - More functions, more nested loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* print_most_numbers - That script prints 10 times the numbers, from 0 to 14, followed by a new line.
+* print_most_numbers - script prints 10 times the numbers, from 0 to 14, followed by a new line.
 * Return: void = 0
 */
 void more_numbers(void)
 {
 	int i, j;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
