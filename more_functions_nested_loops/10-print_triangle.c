c6f745666b7bdf4ac252d1b86bc32025c0c0bdd9
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 10:41:43 -0700

    Commit for C - More functions, more nested loops

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle aligned right, using '#'
+ * @size: Size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		or (j = 0; j < size - i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
