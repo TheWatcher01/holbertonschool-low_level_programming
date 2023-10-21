52b67d3d9b40daf9a983a6f027edf4d279b553de
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 09:45:02 -0700

    Commit for C - More functions, more nested loops

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - printing diagonal line
+ * @n: is the number of times the character \ should be printed
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+		
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
