34352bebcc7f9e897fb2b73b5922432e9f0bf23c
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 09:01:27 -0700

    Commit for C - More functions, more nested loops

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,20 @@
  * Return: void
  */
 
-void print_line(int n) {
-  if (n <= 0) {
-    _putchar('\n');
-  } else {
-    int i;
-    for (i = 0; i < n; i++) {
-      _putchar('_');
-    }
-    _putchar('\n');
-  }
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
