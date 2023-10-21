a88be2c12980edfeae4db534aa8833af2afa4d9c
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 08:24:48 -0700

    Commit for C - More functions, more nested loops

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
+int _putchar(char c)
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
