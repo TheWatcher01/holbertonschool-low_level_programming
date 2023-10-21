7516ebb858150fce414a56714bf7ef725143c9bf
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 09:52:59 -0700

    Commit for C - More functions, more nested loops

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Write function that prints square, followed by new line
+ * @size: size of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		
+		_putchar('\n');
+	} 
+	else
+	{
+		int i, j;
+    
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
