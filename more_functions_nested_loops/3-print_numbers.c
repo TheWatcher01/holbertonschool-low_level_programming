51e043b0a7f0467e165876353e857b374d2336d7
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 07:15:09 -0700

    Commit for C - More functions, more nested loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*print_numbers - Script that prints 0123456789, followed by a new line
+*Return: void
+*/
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10.; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
