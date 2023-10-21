bcd77d1f5e40c1132ce816495f68724687d2fd2d
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-21 08:04:09 -0700

    Commit for C - More functions, more nested loops

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers - Script that prints moster numbers
+*
+* Return: void
+*/
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+}
