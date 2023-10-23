4690410cda24d3b1f48ffc29b3fc8a7d2ca86cce
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-23 06:50:49 -0700

    commit for pointers and arrays courses

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
