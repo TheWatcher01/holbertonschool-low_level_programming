dca6541790d4c6c5581bfd661880dd5393336af0
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-24 02:09:20 -0700

    The next commit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,11 @@ void rev_string(char *s)
 		len++;
 	}
 
-	for (int i = 0, i < len / 2; i++)
+	for (int i = 0; i < len / 2; i++)
 	{
 		char temp = s[i];
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = temp;
+
 	}
 }
