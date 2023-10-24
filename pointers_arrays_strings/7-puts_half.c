b3486585136becfb0002ea62a31e5059264033dd
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-24 07:45:24 -0700

    The next commit

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half(char *str) - function that prints half of a string
+ * followed by a new line.
+ * @str: printed strings 
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	else
+	{
+		for (i = (len + 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+
+	_putchar('\n');
+}
