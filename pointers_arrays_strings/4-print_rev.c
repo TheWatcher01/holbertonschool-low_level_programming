cf20539e0a4f1a35e6a0650ab047329e23334e32
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-24 01:29:55 -0700

    The next commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+/**
+ * print_rev - function that prints a string, in reverse, with a new line.
+ * @s: that print a string in reverse
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	while (len > 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+	_putchar('\n');
+}
