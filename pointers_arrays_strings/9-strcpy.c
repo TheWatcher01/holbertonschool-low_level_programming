e2de9a91f62b9f15271638fda6eb66ac80cf9b9a
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-25 05:51:03 -0700

    the next commit

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies a string
+ * @dest: destination of strings pointed
+ * @src: source 
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int pchar;
+	int len_src = 0;
+
+	while (src[len_src] != '\0')
+	{
+		len_src++;
+	}
+
+	for (pchar = 0; pchar <= len_src; pchar++)
+	{
+		dest[pchar] = src[pchar];
+	}
+	return (dest);
+}
