11b173f5ad0dcb2455f0a4e8b964cc28aff9aff6
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-27 00:40:01 -0700

    next commit

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: string dest
+ * @src: string src
+ * @n: number of elements to concatenate in 
+ * Return: pointer to resulting dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *new_dest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+
+	return (new_dest);
+}
