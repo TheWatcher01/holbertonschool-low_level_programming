501b82530c5a2aca74f6393bf54bb9afe5a72cc0
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-30 03:45:41 -0700

    the next

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: String to be printed in reverse
+ * 
+ * The function works by recursively calling itself to print
+ * the rest of the string,
+ * starting from the end. Once it reaches the end of the string, it prints the
+ * current character and returns.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ * @s: String
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
+}
