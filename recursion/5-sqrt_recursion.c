fd16644daa6b693ed524d5ed14e45c322f759de1
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-31 04:05:11 -0700

    nxt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -6,29 +6,35 @@
  * Return: -1
  */
 
+int calculate_sqrt(int n, int sqr)
+{
+	if (sqr * sqr == n)
+	{
+		return (sqr);
+	}
+
+	else if (sqr * sqr > n)
+	{
+		return (-1);
+	}
+
+	else 
+	{
+		return (calculate_sqrt(n, sqr + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: is an integer
+ * Return: -1
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
-	else if (n == 0 || n == 1)
-	{
-		return n;
-	}
-	else 
-	{
-		int i = 1;
-
-		while (i * i < n)
-		{
-			i++;
-		}
-		if (i * i == n)
-			return i;
-		else
-		{
-			return -1;
-		}
-	}
+	return (calculate_sqrt(n, 0));
 }
