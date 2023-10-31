f2996838b760001f66186e0b9964f075fe5f6ed6
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-31 03:26:58 -0700

    new_1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,14 +5,15 @@
  * @s: String to be printed in reverse
  * 
  * The function works by recursively calling itself to print
- * the rest of the string,
- * starting from the end. Once it reaches the end of the string, it prints the
+ * the rest of the string, starting from the end.
+ * Once it reaches the end of the string, it prints the
  * current character and returns.
+ * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
