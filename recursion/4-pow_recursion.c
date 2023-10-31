ac3e0645e341178197be7bb783d877bc60ad989c
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-31 03:54:07 -0700

    nxt

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -11,16 +11,17 @@
 
 int _pow_recursion(int x, int y)
 {
+	int result = 0;
+
 	if (y < 0)
 	{
-		return -1;
+		return (-1);
 	}
-	else if (y == 0)
+	if (y == 0)
 	{
-		return 1;
-	}
-	else 
-	{
-		return x * _pow_recursion(x, y - 1);
-	}
+		return (1);
+	} 
+	result = x * _pow_recursion(x, y - 1);
+
+	return (result);
 }
