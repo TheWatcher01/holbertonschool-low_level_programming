16aa9f555ffcb9a33bc1081259ea9c8a2221ae8b
TheWatcher01 <teddydeberdt@gmail.com>
2023-10-31 03:47:42 -0700

    nxt

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -12,12 +12,11 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0)
+
+	else if (n == 0 || n == 1)
 	{
 		return (1);
 	}
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+	
+	return (n * factorial(n - 1));
 }
