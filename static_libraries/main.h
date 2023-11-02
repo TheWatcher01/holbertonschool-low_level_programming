9c6296120d34e16c7424e8f255fe529f8076c119
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-02 06:17:21 -0700

    static_libraries_repair

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -23,4 +23,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif // MAIN_H
+#endif
