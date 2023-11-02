15f22a3711881f6ac260539b7b300d7eeeedb138
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-02 07:59:21 -0700

    static_libraries_correction

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,6 +1,9 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,3 +24,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
