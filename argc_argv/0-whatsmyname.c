f44cc14c4f100ca6402e44951021b6d8921be1de
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-03 07:29:41 -0700

    argc_argv_0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,15 +3,15 @@
  * main - Point d'entrée du programme
  * @argc: Le nombre d'arguments passés au programme.
  * Cela inclut le nom du programme lui-même.
- * @argv: Un tableau de chaînes de caractères contenant les arguments 
+ * @argv: Un tableau de chaînes de caractères contenant les arguments
  * passés au programme.
  * argv[0] est le nom du programme, argv[1] est le premier argument, etc.
  *
- * Return: Retourne toujours 0. 
+ * Return: Retourne toujours 0.
  * En C, un retour de 0 depuis la fonction main indique que
  * le programme s'est terminé avec succès.
  */
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
