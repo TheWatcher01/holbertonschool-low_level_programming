7e47a426b512f29318f64d2d39330766c859d682
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-10 06:07:50 -0800

    preprocessor_all

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
