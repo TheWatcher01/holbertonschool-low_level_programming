15cbb15a9eed1678657f2800ec461c9a74c22579
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-10 06:00:57 -0800

    preprocessor_1-pi

---
+++ preprocessor/1-main.c
@@ -0,0 +1,18 @@
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    float a;
+    float r;
+
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return (0);
+}
