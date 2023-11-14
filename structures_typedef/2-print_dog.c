92205712e773e94daf47e35741ae84b8722b6df4
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-14 06:04:24 -0800

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -5,6 +5,8 @@
  *
  * @d: A pointer to a struct dog.
  *
+ * Return: void
+ *
  * Description: If d is NULL, the function does nothing. If an element of d is
  * NULL, the function prints (nil) instead of this element. Otherwise, the
  * function prints the name, age, and owner of the dog in the format:
