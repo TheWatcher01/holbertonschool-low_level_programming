1e62c978776b1dc05fd2fdcbadd92253c85c44d3
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-14 05:26:41 -0800

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,7 +18,10 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+
 		printf("Age: %.6f\n", d->age);
+
 		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+
 	}
 }
