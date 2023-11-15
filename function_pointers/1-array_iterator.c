ebc989a041c3b340c9a456cf57c40a23248f469c
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-15 06:08:48 -0800

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -17,8 +17,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && action != NULL)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	
 	}
 }
