6a6d33ee12cb88af0cb51b045175d65bf4503a97
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-16 08:26:15 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,22 +10,22 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 };
 
 int i = 0;
 
 while (ops[i].op != NULL)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
-		i++;
-	}
+{
+	if (strcmp(ops[i].op, s) == 0)
+		return (ops[i].f);
+	i++;
+}
 	printf("Error\n");
 	exit(99);
 }
