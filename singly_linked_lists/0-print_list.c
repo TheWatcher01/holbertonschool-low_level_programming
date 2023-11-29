ed1a19f6f436385a76052497db2d33ab6fcc0ffe
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-29 00:50:58 -0800

    0-print_list.c_betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 
 /**
-print_list - Function that prints all the elements of a list_t list.
-
-@h: A pointer to the first node in the list.
-
-Return: The number of elements in the list.
-
-*/
+ * print_list - Function that prints all the elements of a list_t list.
+ *
+ * @h: A pointer to the first node in the list.
+ *
+ * Return: The number of elements in the list.
+ */
 
 size_t print_list(const list_t *h)
 {
@@ -18,14 +17,16 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
-		}/*If check str in node = NULL = print [0] (nil)*/
+		} /*If check str in node = NULL = print [0] (nil)*/
+
 		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
-		}/*Print str node = NULL else print [len] str*/
-		
+		} /*Print str node = NULL else print [len] str*/
+
 		h = h->next; /*Goes to the next element in the list*/
 		count++; /*Counter increments*/
 	}
+
 	return (count);/*returns the number of elements in the list*/
 }
