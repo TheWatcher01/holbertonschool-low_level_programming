a1c7d371e45691281d0f3cd2926dd2da813f8f7b
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-29 03:25:58 -0800

    next_add

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Function that adds a new node at the end of a list_t list.
+ * @head:
+ * @str:
+ * Return: The address of the new element, or NULL if it failed.
+ */
+
+
