a73e71846e4e649a2a572e7437bb6f458688da53
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-29 07:46:56 -0800

    3-print_all.c_betty

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,42 +1,74 @@
 #include "variadic_functions.h"
+
+void print_int(int);
+void print_float(float);
+void print_char(char);
+void print_string(char *);
 /**
- * print_all - Prints various data types based provided format string.
- * @format: The format string specifying the types of arguments to print.
- * Return: void
+ * print_all - function
+ * @format: a format
+ * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
 	unsigned int i = 0;
-	char *separator = "";
+	char formats[] = "cifs";
+	void (*print)(void)[] = {print_char, print_int,
+		print_float, print_string};
+	va_list argv;
+	void types[] = {char, int, float, char * };
 
-	va_start(args, format);
+	va_start(argv, format);
 
-	while (format[i])
+	while ((formats + i) != NULL)
 	{
-		switch (format[i++])
-		{
-			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
-				separator = ", ";
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
-				separator = ", ";
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
-				separator = ", ";
-				break;
-			case 's':
-				printf("%s%s", separator, va_arg(args, const char *));
-				separator = ", ";
-				break;
-			default:
-				break;
-		}
+
+		if (i != 0)
+			printf(", ");
+		(print[i])(va_arg(argv, types[i]));
 	}
 
-	va_end(args);
+	va_end(argv);
 	printf("\n");
+}
+
+
+/**
+ * print_int - function
+ * @a: a int
+ * Return: Nothing
+ */
+void print_int(int a)
+{
+	printf("%d", a);
+}
+
+/**
+ * print_float - function
+ * @a: float number
+ * Return: Nothing
+ */
+void print_float(float a)
+{
+	printf("%f", a);
+}
+
+/**
+ * print_char - function
+ * @a: char
+ * Return: Nothing
+ */
+void print_char(char a)
+{
+	putchar(a);
+}
+
+/**
+ * print_string - function
+ * @s: a string
+ * Return: Nothing
+ */
+void print_string(char *s)
+{
+	printf("%s", s);
 }
