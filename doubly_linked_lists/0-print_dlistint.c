4cc911053caf065fe9472cf01c28a9112e04d733
TheWatcher01 <teddydeberdt@gmail.com>
2023-11-30 02:28:24 -0800

    0-print_dlistint.c_betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,19 +2,19 @@
 
 /**
  * print_dlistint - Function that prints all the elements of a dlistint_t list
- * h: Pointer to head
+ * @h: Pointer to head
  * Return: the number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t n = 0;
+	size_t counter = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", h->counter);
 		n++;
 		h = h->next;
 	}
-	return (n);
+	return (counter);
 }
