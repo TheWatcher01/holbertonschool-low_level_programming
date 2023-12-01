3501a094475982e9f69a151c2283a453daca794d
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-01 13:23:41 -0800

    final_betty

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits to flip to get from one number to another.
+ * flip_bits - Return number of bits to flip to get from one number to another
  * @n: The first number.
  * @m: The second number.
  *
