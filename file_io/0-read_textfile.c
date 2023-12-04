7edc7ffd0401bb5432deed918132bdc39a557af1
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-04 04:36:14 -0800

    0-read_textfile.c_v4

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,14 +1,10 @@
 #include "main.h"
-
 /**
  * read_textfile - Function reads text file and print to POSIX standard output
- *
  * @filename: Name of the file to read.
  * @letters: Number of letters to read and print.
- *
  * Return: Actual number of letters read and printed, or 0 if an error occurs.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -30,10 +26,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
 	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
 	free(buffer);
 	close(fd);
 
-	return ((n_written == -1 || n_written != n_read) ? 0 : n_written);
+	return (n_written);
 }
