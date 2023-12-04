6e3d28ea90766972995cc6fafac4d746e42a900b
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-04 03:46:32 -0800

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,39 +1,31 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads text file and prints it to POSIX standard output
- * @filename: Name of the file to read
- * @letters: Number of letters to read and print
- * Return: Actual number of letters read and printed, or 0 if error
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the name of the file to read
+ * @letters: the number of letters to read and print
+ * Return: the actual number of letters read and printed, or 0 if error
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *f;
-	int c;
-	size_t count = 0;
+    FILE *f;
+    char c;
+    size_t count = 0;
 
-	if (filename == NULL)
-	{
-		return (0);
-	}
+    if (filename == NULL)
+        return (0);
 
-	f = fopen(filename, "rt");
-	if (f == NULL)
-	{
-		return (0);
-	}
+    f = fopen(filename, "rt");
+    if (f == NULL)
+        return (0);
 
-	while ((c = fgetc(f)) != EOF && count < letters)
-	{
-		if (write(1, &c, 1) != 1)
-		{
-			fclose(f);
-			return (0);
-		}
-		count++;
-	}
+    while ((c = fgetc(f)) != EOF && count < letters)
+    {
+        if (write(1, &c, 1) != 1)
+            return (0);
+        count++;
+    }
 
-	fclose(f);
-	return (count);
+    fclose(f);
+    return (count);
 }
