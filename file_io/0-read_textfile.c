9ad482f8b89bfd8fefdd2aeeb6ea3216e7e38cae
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-04 04:29:19 -0800

    0-read_textfile.c_v3

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,13 +15,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t n_read, n_written;
 
+	if (filename == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
 
-	buffer = malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
@@ -29,23 +30,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	n_read = read(fd, buffer, letters);
-	if (n_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
 	n_written = write(STDOUT_FILENO, buffer, n_read);
-	if (n_written == -1 || n_written != n_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
 
 	free(buffer);
 	close(fd);
 
-	return (n_written);
+	return ((n_written == -1 || n_written != n_read) ? 0 : n_written);
 }
