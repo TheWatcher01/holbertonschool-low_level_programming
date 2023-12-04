00a7ebe2e014b3d356e62c82456061b3fa434108
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-04 04:39:52 -0800

    0-read_textfile.c_v5

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,44 +5,27 @@
  * @letters: Number of letters to read and print.
  * Return: Actual number of letters read and printed, or 0 if an error occurs.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buffer;
-	ssize_t n_read, n_written;
+	FILE *f;
+	char c;
+	size_t count = 0;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	f = fopen(filename, "rt");
+	if (f == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	while ((c = fgetc(f)) != EOF && count < letters)
 	{
-		close(fd);
-		return (0);
+		if (write(1, &c, 1) != 1)
+			return (0);
+		count++;
 	}
 
-	n_read = read(fd, buffer, letters);
-	if (n_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	n_written = write(STDOUT_FILENO, buffer, n_read);
-	if (n_written == -1 || n_written != n_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	free(buffer);
-	close(fd);
-
-	return (n_written);
+	fclose(f);
+	return (count);
 }
