9140ed6fa3e53b92d76a2e7decd19eb87cdd33d6
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-05 03:54:42 -0800

    3-cp.c_v8

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,52 +7,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_to, fd_from;
+	int fd_from, fd_to, read_from;
 	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
+	ssize_t write_to;
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from fome_to\n");
 		exit(97);
 	}
-
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-	if (fd_to == -1)
+	open_files(&fd_from, &fd_to, argv);
+	while ((read_from = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		perror("Error: Can't create file");
-		exit(99);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		perror("Error: Can't read file");
-		exit(98);
-	}
-
-	while (1)
-	{
-		bytes_read = read(fd_from, buffer, sizeof(buffer));
-		if (bytes_read == 0)
-			break;
-		else if (bytes_read == -1)
+		write_to = write(fd_to, buffer, read_from);
+		if (write_to != read_from)
 		{
-			perror("Error: Can't read file");
-			exit(98);
-		}
-
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			perror("Error: Can't write to file");
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 			exit(99);
 		}
 	}
-
-	close(fd_from);
-	close(fd_to);
-
+	if (read_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	close_files(fd_from, fd_to);
 	return (0);
 }
