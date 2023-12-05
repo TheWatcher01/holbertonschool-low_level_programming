a197de392aadd798b2dc203a3fc340092768b336
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-05 04:14:02 -0800

    3-cp.c_v10

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,14 +27,14 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((read_from = read(fd_from, buffer, BUFFER_SIZE)) > 0)
