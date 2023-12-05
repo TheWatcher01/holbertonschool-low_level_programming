20a94f26ce2e455a33ef9114d4327c2885ed4cb5
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-05 01:42:40 -0800

    3-cp.c_v1 + add 3-diy_open_files.c 3-diy_close_files.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,20 +1,10 @@
 #include "main.h"
-
-#define BUFFER_SIZE 1024
-
 /**
  * main - Copies content of a file to another file
  * @argc: Argument count
  * @argv: Array of arguments, argv[1] is source, argv[2] is destination
- *
- * The program reads from source and writes to destination.
- * If destination exists, it's truncated. If not, it's created.
- * The program reads source in 1024 byte chunks and writes to destination.
- * If error occurs during read, write, or close, it prints an error and exits.
- *
  * Return: 0 on success, non-zero on failure
  */
-
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, read_from;
@@ -26,20 +16,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from fome_to\n");
 		exit(97);
 	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-		exit(99);
-	}
+	open_files(&fd_from, &fd_to, argv);
 	while ((read_from = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		write_to = write(fd_to, buffer, read_from);
@@ -54,18 +31,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-
+	close_files(fd_from, fd_to);
 	return (0);
 }
---
+++ file_io/3-diy_close_files.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * close_files - Closes the source and destination files
+ * @fd_from: File descriptor of the source file
+ * @fd_to: File descriptor of the destination file
+ */
+void close_files(int fd_from, int fd_to)
+{
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+}
