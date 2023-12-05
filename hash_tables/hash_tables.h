7d00c5930d9d3ad960000d7050200e4f21ec296e
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-05 06:03:29 -0800

    betty

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -4,23 +4,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-/* Structures Prototypes*/
 
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size, each cell of this array is a pointer to the
+ * first node of a linked list, because we want our HashTable to use a Chaining
+ * collision handling
+ */
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
-/* Functions Prototypes*/
-
+/* Function Prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
